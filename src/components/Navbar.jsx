e9e4e90cc456de86c371eb31d0229ae2599a8cc6
michelleree <michelle.rickenmann@gmail.com>
2025-09-18 20:40:27 +0200

    changed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,39 @@
-import {useState} from "react";
-import {Link} from "react-router-dom";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
 
 function Navbar() {
-    const [isNavActive, setIsNavActive] = useState(false);
+    const [isNavOpen, setIsNavOpen] = useState(false);
 
     const toggleNav = () => {
-        setIsNavActive(!isNavActive);
+        setIsNavOpen(!isNavOpen);
     };
 
     return (
-        <nav>
-            <div className="logo">
-                {/*<img src="assets/Logo64x64.png" alt="logo"/>*/}
-                <h1>m.</h1>
-            </div>
-            <ul className={`menubar ${isNavActive ? "active" : ""}`}>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/work">Work</Link>
-                </li>
-                <li>
-                    <Link to="/experience">Experience</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-            </ul>
+        <nav className="navbar-container">
+            <h1 className="logo">m.</h1>
             <div
-                className={`hamburger ${isNavActive ? "hamburger-active" : ""}`}
+                className={`hamburger-lines ${isNavOpen ? "hamburger-active" : ""}`}
                 onClick={toggleNav}
             >
-                <span className="line"></span>
-                <span className="line"></span>
-                <span className="line"></span>
+                <span className="line line1"></span>
+                <span className="line line2"></span>
+                <span className="line line3"></span>
             </div>
+            <ul className={`menu-items ${isNavOpen ? "active" : ""}`}>
+                <li>
+                    <Link to="/" onClick={toggleNav}>Home</Link>
+                </li>
+                <li>
+                    <Link to="/work" onClick={toggleNav}>Work</Link>
+                </li>
+                <li>
+                    <Link to="/experience" onClick={toggleNav}>Experience</Link>
+                </li>
+                <li>
+                    <Link to="/contact" onClick={toggleNav}>Contact</Link>
+                </li>
+            </ul>
         </nav>
     );
 }
