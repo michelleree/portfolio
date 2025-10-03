216f327dd20fccefc230e33dfaf0449c0245212b
Michelle Rickenmann <michelle.rickenmann@gmail.com>
2025-10-03 21:09:29 +0200

    work expanded

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -5,24 +5,30 @@ import filmStripe from "../assets/images/film stripe.png";
 export default function Work() {
     return (
         <main>
-            <h2>My Work & Experience</h2>
-            <div>
-                <img src={filmStripe} alt="Film Stripe"/>
-            </div>
-            <div>
-                <p>Take a look at my work from university as well as some personal projects.
-                    Keep in my mind that for all my work I use AI in many different ways to help me. None of my work is
-                    done 100% with AI, but I use it for coding and to help me generate ideas.
-                    If you have any questions or concerns, feel free to reach out.</p>
-            </div>
-            <div>
-                <img src={filmStripe} alt="Film Stripe"/>
-            </div>
-            <div className="projects-overview">
+            <section className="work-intro">
+                <h2>My Work & Experience</h2>
+                <div>
+                    {/*<img src={filmStripe} alt="Film Stripe"/>*/}
+                    <div className="stripe1"></div>
+                </div>
+                <div>
+                    <p>Take a look at my work from university as well as some personal projects.
+                        Keep in my mind that for all my work I use AI in many different ways to help me. None of my work
+                        is
+                        done 100% with AI, but I use it for coding and to help me generate ideas.
+                        If you have any questions or concerns, feel free to reach out.</p>
+                </div>
+                <div>
+                    {/*<img src={filmStripe} alt="Film Stripe"/>*/}
+                    <div className="stripe2"></div>
+                </div>
+            </section>
+            <section className="projects-overview">
+                <h3>Design</h3>
                 <div className="project">
 
                 </div>
-            </div>
+            </section>
             <div>
                 <img src={filmStripe} alt="Film Stripe"/>
             </div>
